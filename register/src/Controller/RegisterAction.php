8d04a6aa119a12832ee864cef98652e42f35a8a5
bryvid09 <tipan88@gmail.com>
2024-08-17 23:49:06 +0200

    create messenger message rabbitmq

--- register/src/Controller/RegisterAction.php
+++ register/src/Controller/RegisterAction.php
@@ -5,14 +5,20 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Http\DTO\RegisterRequest;
+use App\Service\RegisterActionService;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RegisterAction
 {
+    public function __construct(RegisterActionService $registerActionService)
+    {
+        $this->registerActionService = $registerActionService;
+    }
     #[Route('/api/register', name: 'api_register', methods: ['POST'])]
     public function __invoke(RegisterRequest $registerRequest): JsonResponse
     {
+        ($this->registerActionService)($registerRequest->getName(), $registerRequest->getEmail());
         return new JsonResponse();
     }
 
